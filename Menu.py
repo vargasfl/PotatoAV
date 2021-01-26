ad48388ea818dcf07f49fada73e5771093efe830
gerardovigil <workgv45@gmai.com>
2021-01-26 03:56:26 -0800

    added message incase user gives an incorrect input added option to return to Main menu

--- Menu.py
+++ Menu.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import checker
 import sys
+import time
 def menu():
   art_file = open('./ASCII_potatopic.txt')
   print(art_file.read())
@@ -10,7 +11,9 @@ def menu():
   print('[1]: Scan')
   print('[2]: About')
   print('[3]: Quit')
+
   response = input()
+  	
   if response == '2':
       print()
       print(open('./README.md').read())
@@ -19,7 +22,26 @@ def menu():
   if response == '3':
       print()
       print('Bye-bye!')
-      exit()
+   
+   # incorrect key message and program continuation until system scanned or program quit    
+  response_opt = ['1','2','3']
+  while response not in response_opt:
+  	print('oops..wrong key. please enter option 1,2,or 3...')
+  	time.sleep(3)
+  	return menu()
+  		
+  if response != '3':
+  	print('please Enter M to return to the main menu !')
+  	print(' or enter any key to exit')
+  	
+  	response2 = input()
+  	if response2 == 'M' or response2 == 'm':
+  		return menu()
+  	if response2 != 'M' or response2 != 'm':
+  		print('Bye-bye!')
+  		exit()
+  	exit()
+  	
 def main():
     menu()
 main()
