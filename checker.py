a873895844712871455d1181a225fb47e3de0558
vargasfl <flvargas23@gmail.com>
2021-01-30 10:38:20 -0800

    Not quite there, but so close
    Function is weird in text editor, getting really close, but going to test in VM to see if it works differently there.

--- checker.py
+++ checker.py
@@ -3,31 +3,26 @@
 import os
 import hashlib
 import uh_oh
+from vt import VT
+vt = VT()
+vt.setkey('0a1c6042e479633d58c7123ab825510f9d2fccda16d13c7b553c1dcf047b3425')
+import time
 
 def check_file():
-	malicious_check = open('./bad_hashes.txt')
-	malicious_list = malicious_check.read()
 	file_list = []
 	hash_list = []
+	vt_results = {}
 	for root,dirs,files in os.walk ('.'):
 		for filename in sorted(files):
 			full_path = os.path.join(root,filename)
-			full_open = open(full_path, 'rb')
-			full_read = full_open.read()
-			full_open.close()
-			mdholder = hashlib.md5(full_read).hexdigest()
-			sha256holder = hashlib.sha256(full_read).hexdigest()
-			sha512holder = hashlib.sha512(full_read).hexdigest()
-			if mdholder in malicious_list:
-				file_list.append(full_path)
-				hash_list.append(mdholder)
-			elif sha256holder in malicious_list:
-				file_list.append(full_path)
-				hash_list.append(sha256holder)
-			elif sha512holder in malicious_list:
-				file_list.append(full_path)
-				hash_list.append(sha512holder)
-	print(file_list)
+			#full_open = open(full_path, 'rb')
+			#full_read = full_open.read()
+			#mdholder = hashlib.md5(full_read).hexdigest()
+			print(filename)
+			vt_results = vt.getfile(full_path)
+			print(vt_results)
+			#print(mdholder)
+			time.sleep(30)
 	if len(file_list) == 0:
 		print('Wow! Your system is completely clean (as far as I can tell).')
 		print('Thanks for using PotatoAV, for more information check out https://github.com/vargasfl/PotatoAV')
