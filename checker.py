c33f3537ca8e39d8dc6a88652d6bc662a59920bd
vargasfl <flvargas23@gmail.com>
2021-02-01 19:34:07 -0800

    updates again

--- checker.py
+++ checker.py
@@ -3,8 +3,8 @@
 import os
 import hashlib
 import uh_oh
-from vt import VT
-vt = VT()
+import __init__
+vt = __init__.VT()
 vt.setkey('0a1c6042e479633d58c7123ab825510f9d2fccda16d13c7b553c1dcf047b3425')
 import time
 
@@ -12,6 +12,7 @@ def check_file():
 	file_list = []
 	hash_list = []
 	vt_results = {}
+        key_we_want = ['response_code', 'positives']
 	comparison = 'Scan request successfully queued, come back later for the report'
 	for root,dirs,files in os.walk ('.'):
 		for filename in sorted(files):
@@ -20,9 +21,18 @@ def check_file():
 			full_read = full_open.read()
 			mdholder = hashlib.md5(full_read).hexdigest()
 			vt_results = vt.getfile(mdholder)
+                        thisbetterwork = [vt_results[key] for key in key_we_want]
 			print(full_path)
+                        if thisbetterwork[0] == 0:
+                            print("Sorry pal, Virustotal does not like this one")
 			time.sleep(15)
-			print(vt.getfile(mdholder))
+                        elif thisbetterwork[0] == 1:
+		            if thisbetterwork[1] == 0:
+                                print('This one is good')
+                            elif 1 <= thisbetterwork[1] <= 3:
+                                print('Get a load of this guy')
+                            elif thisbetterwork[1] > 3:
+                                print('WEEWOOWEEWOO')
 			time.sleep(15)
 	if len(file_list) == 0:
 		print('Wow! Your system is completely clean (as far as I can tell).')
